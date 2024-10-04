e48cce793f0cf4ce67d1181ec9e425b9bfe75c0d
Joe Joseph <atj12j@gmail.com>
2024-10-04 16:07:24 +0530

    Add files via upload
    Here is my first python project

---
+++ EZE.py
@@ -0,0 +1,43 @@
+'''
+print("hello\nworld")
+if 5>2:
+    print("five is greater than two")
+
+    if 5>2:
+        print("five is greater than two")
+x=5
+x="thomas"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+print(x,y,z)
+
+x=5j
+y=("john")
+print(type(x))
+print(type(y)
+
+from os.path import isabs
+
+x = "python "
+y = "is "
+z = "awesome "
+print(x + y+ z)
+
+username = input("enter username:")
+print("username is: " + username)
+
+a = "hello "
+b = "world"
+c = a+b
+print(c)
+'''
+a= " hello world "
+print(a[8])
+b= " hello world"
+print(b[:5])
