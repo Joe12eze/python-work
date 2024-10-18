76cb9b0dda079ae346d77223d83a00fb8b1a30f1
Joe Joseph <atj12j@gmail.com>
2024-10-18 14:34:48 +0530

    Add files via upload

---
+++ basicinputoperations.py
@@ -0,0 +1,9 @@
+"""
+Author: Joe Joseph
+Date : 18-10-2024
+Description :program that asks the user to input their name and age.
+ After receiving the input, display a message that includes the user's name and age."""
+
+name=input("enter your name:")
+age=input("enter your age:")
+print("hello,",name+"! you are ",age,"years old")
