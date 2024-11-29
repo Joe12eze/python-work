c46bec4e652e961df36582b73c089b859c1baecd
Joe Joseph <joejoseph2028@cs.sjcetpalai.ac.in>
2024-11-29 15:00:11 +0530

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -1,5 +1,18 @@
-str1="Hello, World!"
-str2=str1[7:13]
-str3=" Everyone!"
-str4=str1[7:12]+str3
-print(str4)
+"""Author: Joe Joesph
+Description:A program that accepts the lengths of three sides of a triangle as inputs.
+ The program should output whether or not the triangle is a right triangle .
+Implement using functions.
+"""
+def check_right_triangle(sides):
+    sides.sort()
+    if sides[2]**2 == sides[0]**2 + sides[1]**2:
+            return True
+    return False
+sides=[]
+sides.append(int(input("Enter side 1:")))
+sides.append(int(input("Enter side 2:")))
+sides.append(int(input("Enter side 3:")))
+if check_right_triangle(sides):
+    print("The given sides are part of right triangle")
+else:
+    print("The given sides are not part of right triangle")
